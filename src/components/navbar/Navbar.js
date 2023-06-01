bbe7101c6daa458f18c44dc75f2b8dbb3d78e0f9
saket kumar <saketk89@gmail.com>
2023-06-02 02:10:28 +0530

    feat - started Order page

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import "./Navbar.css"
+import "./Navbar.css";
+import { Link } from 'react-router-dom';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='nav-left'>
+        <h1 className='head-text'><FastfoodIcon style={{fontSize:'35px',backgroundColor:"black"}}/>FOODAHOLIC</h1>
+      </div>
+      <div className='nav-right'>
+        <Link  to="/orders" style={{textDecoration:"none",backgroundColor:"black",}}>
+          <p >ORDERS</p>
+        </Link>
+        <Link to="/addfood" style={{textDecoration:"none",backgroundColor:"black"}}>
+          <p>ADD FOOD</p>
+        </Link>
+      </div>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/components/orders/OrderSection.js
+++ src/components/orders/OrderSection.js
@@ -1,12 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./OrderSection.css";
 import Navbar from "../navbar/Navbar";
+import {getDocs,collection,doc,setDoc} from "firebase/firestore";
+import {db,storage} from "../../firebase/FirebaseConfig"
 
 const OrderSection = () => {
+  const [allOrders,setAllOrders]= useState([]);
+  const [allOrdersStatus,setAllOrdersStatus]= useState([]);
+  const [keyword,setKeyword]= useState([]);
+
+  const getAllOrder = async () => {
+    setAllOrders([]);
+    const querySnapshot = await getDocs(collection(db,"UserOrders"));
+    querySnapshot.forEach((doc)=>{
+      console.log(doc.id,"=>",doc.data())
+      setAllOrders((prev)=>[...prev,doc.data()])
+    })
+  };
+
+  useEffect(()=>{
+    getAllOrder();
+  },[])
+
   return (
-    <div>
+    <div className="order-section">
       <Navbar />
-      OrderSection
+      <h1 className="order-head1">ORDER - SECTION</h1>
+      <div className="order-s1">
+        <input
+          type="text"
+          placeholder="Search by order id or delivery status"
+          className="search-bar"
+        />
+        <div className="order-s1-in">
+          <p>Sort by Order Status</p>
+          <select className="order-status-text">
+            <option value="">All</option>
+            <option value="pending">Pending</option>
+            <option value="ontheway">On the way</option>
+            <option value="delivered">Delivered</option>
+            <option value="cancelled">Cancelled</option>
+          </select>
+        </div>
+      </div>
+      <div className="order-container">
+        <div className="order-row-card1">
+          <p className="order-text">Order Id</p>
+          <p className="order-text">Paid</p>
+          <p className="order-text">Delivery Status</p>
+          <p className="order-text">Delivery Boy Name</p>
+          <p className="order-text">Delivery Boy Phone</p>
+
+          <p className="order-text">Cost</p>
+          <button>Show Details</button>
+        </div>
+        <div className="order-container">
+          {/** DATA */}
+          {
+            allOrders.map((order)=>{
+              return(
+                <div className="order-row-card"></div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 };
