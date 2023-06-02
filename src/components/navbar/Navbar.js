0da1d2c8401ea6ca74233bfb437811715dd61518
saket kumar <saketk89@gmail.com>
2023-06-02 18:04:12 +0530

    feat - completed delivery boy name and phone im plementation

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React from "react";
 import "./Navbar.css";
-import { Link } from 'react-router-dom';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
+import { Link } from "react-router-dom";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import image from "../../assets/black-texture.jpg"
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className='nav-left'>
-        <h1 className='head-text'><FastfoodIcon style={{fontSize:'35px',backgroundColor:"black"}}/>FOODAHOLIC</h1>
+    <div className="navbar" style={{ backgroundImage:`url(${image})`,backgroundRepeat:"repeat" }}>
+      <div className="nav-left">
+        <div className="head-container">
+          <FastfoodIcon
+            style={{ fontSize: "40px", backgroundColor: "transparent",marginRight:"10px" }}
+          />
+          <h1 className="head-text">FOODAHOLIC</h1>
+        </div>
       </div>
-      <div className='nav-right'>
-        <Link  to="/orders" style={{textDecoration:"none",backgroundColor:"black",}}>
-          <p >ORDERS</p>
+      <div className="nav-right">
+        <Link
+          to="/orders"
+          style={{ textDecoration: "none", backgroundColor: "transparent" }}
+        >
+          <p>ORDERS</p>
         </Link>
-        <Link to="/addfood" style={{textDecoration:"none",backgroundColor:"black"}}>
+        <Link
+          to="/addfood"
+          style={{ textDecoration: "none", backgroundColor: "transparent" }}
+        >
           <p>ADD FOOD</p>
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
--- src/components/orders/OrderSection.js
+++ src/components/orders/OrderSection.js
@@ -1,29 +1,76 @@
 import React, { useEffect, useState } from "react";
 import "./OrderSection.css";
 import Navbar from "../navbar/Navbar";
-import {getDocs,collection,doc,setDoc} from "firebase/firestore";
-import {db,storage} from "../../firebase/FirebaseConfig"
+import { getDocs, collection, doc, setDoc } from "firebase/firestore";
+import { db, storage } from "../../firebase/FirebaseConfig";
 
 const OrderSection = () => {
-  const [allOrders,setAllOrders]= useState([]);
-  const [allOrdersStatus,setAllOrdersStatus]= useState([]);
-  const [keyword,setKeyword]= useState([]);
+  const [allOrders, setAllOrders] = useState([]);
+  const [allOrdersStatus, setAllOrdersStatus] = useState([]);
+  const [keyword, setKeyword] = useState([]);
 
   const getAllOrder = async () => {
     setAllOrders([]);
-    const querySnapshot = await getDocs(collection(db,"UserOrders"));
-    querySnapshot.forEach((doc)=>{
-      console.log(doc.id,"=>",doc.data())
-      setAllOrders((prev)=>[...prev,doc.data()])
-    })
+    const querySnapshot = await getDocs(collection(db, "UserOrders"));
+    querySnapshot.forEach((doc) => {
+      // console.log(doc.id, "=>", doc.data());
+      setAllOrders((prev) => [...prev, doc.data()]);
+    });
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getAllOrder();
-  },[])
+  }, []);
+  
+  const changeOrderStatus = (id,orderData,status) => {
+    const docRef = doc(db,"UserOrders",id);
+    const data = {
+      ...orderData,
+      orderStatus:status
+    }
+    setDoc(docRef,data)
+    .then(()=>{
+      alert("Status updated Successfully")
+    })
+    .catch((error)=>{
+      alert("Failed to update status",error);
+    })
+    getAllOrder();
+  }
+  const changeDeliveryBoyName = (id,orderData,name) => {
+    const docRef = doc(db,"UserOrders",id);
+    const data = {
+      ...orderData,
+      deliveryBoyName:name
+    }
+    setDoc(docRef,data)
+    .then(()=>{
+      alert("Delivery Boy name updated Successfully")
+    })
+    .catch((error)=>{
+      alert("Failed to update Delivery Boy name",error);
+    })
+    getAllOrder();
+  }
+  const changeDeliveryBoyphone = (id,orderData,phone) => {
+    const docRef = doc(db,"UserOrders",id);
+    const data = {
+      ...orderData,
+      deliveryBoyPhone:phone
+    }
+    setDoc(docRef,data)
+    .then(()=>{
+      alert("Delivery boy Phone updated Successfully")
+    })
+    .catch((error)=>{
+      alert("Failed to update Delivery boy Phone",error);
+    })
+    getAllOrder();
+  }
 
+  console.log(allOrders);
   return (
-    <div className="order-section">
+    <div className="order-section" >
       <Navbar />
       <h1 className="order-head1">ORDER - SECTION</h1>
       <div className="order-s1">
@@ -34,7 +81,7 @@ const OrderSection = () => {
         />
         <div className="order-s1-in">
           <p>Sort by Order Status</p>
-          <select className="order-status-text">
+          <select className="order-status-text" >
             <option value="">All</option>
             <option value="pending">Pending</option>
             <option value="ontheway">On the way</option>
@@ -56,13 +103,78 @@ const OrderSection = () => {
         </div>
         <div className="order-container">
           {/** DATA */}
-          {
-            allOrders.map((order)=>{
-              return(
-                <div className="order-row-card"></div>
-              )
-            })
-          }
+          {allOrders.map((order) => {
+            return <div className="order-row-card">
+              <p className="order-text">{order.orderid}</p>
+              <p className="order-text">{order.orderPayment}</p>
+              <div className="order-card-in">
+                {
+                  order.orderStatus === "pending" && 
+                  <select onChange={(e)=>{
+                    changeOrderStatus(order.orderid,order,e.target.value)
+                    }}>
+                  <option value="pending">Pending</option>
+                  <option value="ontheway">On the way</option>
+                  <option value="delivered">Delivered</option>
+                  <option value="cancelled">Cancelled</option>
+                  </select>
+                }
+                {
+                  order.orderStatus === "ontheway" && 
+                  <select onChange={(e)=>{
+                    changeOrderStatus(order.orderid,order,e.target.value)
+                    }}>
+                  <option value="pending">Pending</option>
+                  <option value="ontheway">On the way</option>
+                  <option value="delivered">Delivered</option>
+                  <option value="cancelled">Cancelled</option>
+                  </select>
+                }
+                {
+                  order.orderStatus === "delivered" && 
+                  <select onChange={(e)=>{
+                    changeOrderStatus(order.orderid,order,e.target.value)
+                    }}>
+                  <option value="delivered">Delivered</option>
+                  <option value="pending">Pending</option>
+                  <option value="ontheway">On the way</option>
+                  <option value="cancelled">Cancelled</option>
+                  </select>
+                }
+                {
+                  order.orderStatus === "cancelled" && 
+                  <p className="order-text">{order.orderStatus}</p>
+                }
+              </div>
+              
+              { // DELIVERY BOY NAME
+                order.deliveryBoyName ?  
+                <p className="order-text">{order.deliveryBoyName}</p>:
+                <input 
+                   type="text" 
+                   placeholder="Enter Name" 
+                   className="order-input" 
+                   onBlur={(e)=>{
+                    changeDeliveryBoyName(order.orderid,order,e.target.value)
+                   }}
+                />
+              }
+              { // DELIVERY BOY PHONE
+                order.deliveryBoyPhone ?  
+                <p className="order-text">{order.deliveryBoyPhone}</p>:
+                <input 
+                   type="text" 
+                   placeholder="Enter phone" 
+                   className="order-input"
+                   onBlur={(e)=>{
+                    changeDeliveryBoyphone(order.orderid,order,e.target.value)
+                   }}
+                   />
+              }
+              <p className="order-text">{order.orderCost}</p>
+              <button className="show-detail-button">Show Details</button>
+            </div>;
+          })}
         </div>
       </div>
     </div>
