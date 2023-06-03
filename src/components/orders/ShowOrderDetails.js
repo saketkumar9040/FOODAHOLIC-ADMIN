5c50449230717360f66e904916982679eb3504af
saket kumar <saketk89@gmail.com>
2023-06-03 18:30:22 +0530

    feat - implemented background image in order section and fetch orderData from firestore

--- src/components/orders/ShowOrderDetails.js
+++ src/components/orders/ShowOrderDetails.js
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./ShowOrderDetails.css";
-import  Navbar from "../navbar/Navbar";
-import { Link } from 'react-router-dom';
+import Navbar from "../navbar/Navbar";
+import { Link, useParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase/FirebaseConfig";
+import orderDetailsBackground from "../../assets/order-details-background.jpg"
 
-const showOrderDetails = () => {
+const ShowOrderDetails = () => {
+  const { orderid } = useParams();
+  console.log(orderid);
+
+  const [orderData, setOrderData] = useState([]);
+
+  const getOrderData = async () => {
+    const docRef = await doc(db, "UserOrders", orderid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      console.log(docSnap.data());
+      setOrderData(docSnap.data());
+    } else {
+      console.log("No such document exists");
+    }
+  };
+
+  useEffect(() => {
+    getOrderData();
+  }, []);
+
+  console.log(orderData);
+
   return (
-    <div className='order-section'>
-      <Navbar/>
-      <Link to="/orders" style={{marginTop:"10px"}}>
-        <button className='go-back-button'>Go Back</button>
-      </Link>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="order-section"style={{ backgroundImage:`url(${orderDetailsBackground})`,
+    backgroundRepeat:"no-repeat",backgroundSize:"cover",height:"100%"}}>
+        <Link to="/orders" style={{backgroundColor:"transparent"}}>
+          <button className="go-back-button">Go Back</button>
+        </Link>
+        <h1 className="order-heading">ORDER - DETAILS</h1>
+        <div className="order-details-form">
+          <div className="order-details-row">
+            <p>Customer Name</p>
+            <p>{orderData.ordererName}</p>
+          </div>
+          <div className="order-details-row">
+            <p>Customer Address</p>
+            <p style={{alignSelf:"flex-end",marginLeft:"280px"}}>{orderData.ordererAddress}</p>
+          </div>
+          <div className="order-details-row">
+            <p>Customer Phone</p>
+            <p>{orderData.ordererPhone}</p>
+          </div>
+          <div className="order-details-row">
+            <p>Order status</p>
+            <p>{orderData.orderStatus}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default showOrderDetails
+export default ShowOrderDetails;
